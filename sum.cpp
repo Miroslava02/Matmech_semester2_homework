0fd18c71a34b85f818ebbde033732e28a94ff5ea
Miroslava02 <71526710+Miroslava02@users.noreply.github.com>
2021-03-31 16:06:03 +0300

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+int sum(int a, int b){
+    return a+b;
+}
+
+
+int main(){
+    cout<<"hello world";
+    int a, b, a1, b1;
+    int line;
+    vector <int> A, B;
+    ifstream in("C:\\Users\\Мирослава\\OneDrive\\Документы\\input.txt");
+    if (in.is_open())
+    {
+        while (in>>a>>b)
+        {
+           A.push_back(a);
+           B.push_back(b);
+           
+        }
+    }
+    in.close(); 
+    for (int i=0; i<3; i++){
+        cout<<sum(A[i], B[i]);
+    }
+    system("PAUSE");  
+    return 0;
+}
