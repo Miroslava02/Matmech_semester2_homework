755034ab9ff6de43b9c7c3fc8ebd86d14d7653b4
Miroslava02 <71526710+Miroslava02@users.noreply.github.com>
2021-11-18 11:27:52 +0300

    Add files via upload

--- kernel.cu
+++ kernel.cu
@@ -6,8 +6,10 @@
 #include<iostream>
 using namespace std;
 
-#define N 80000 //количество элементов массива
+#define N  10000//количество элементов массива
 // Ядро
+
+#define M 7  //тво массивов
 
 __global__ void add(int* a, int* b, int* c)
 {
@@ -19,31 +21,35 @@ __global__ void add(int* a, int* b, int* c)
 	c[tid] = a[tid] + b[tid];
 }
 int main() {
-	 double start_time = clock();
+	double start_time = clock();
 	// выделение памяти под массивы на CPU
-	int host_a[N], host_b[N], host_c[N];
+	int host_a[M][N];
+	int host_b[M][N];
+	int host_c[M][N];
 	// выделение памяти под массивы для копирования
 	// на GPU
 	int* dev_a, * dev_b, * dev_c;
 	// заполнение массивов
-	for (int i = 0; i < N; i++)
-	{
-		host_a[i] = 23;
-		host_b[i] = 23;
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) 
+		{
+			host_a[i][j] = 23;
+			host_b[i][j] = 23;
+		}
 	}
 	// выделение памяти под массивы на GPU
-	cudaMalloc((void**)&dev_a, N * sizeof(int));
-	cudaMalloc((void**)&dev_b, N * sizeof(int));
-	cudaMalloc((void**)&dev_c, N * sizeof(int));
+	cudaMalloc((void**)&dev_a, N * M * sizeof(int));
+	cudaMalloc((void**)&dev_b, N * M * sizeof(int));
+	cudaMalloc((void**)&dev_c, N * M * sizeof(int));
 	// копирование данных в память GPU
-	cudaMemcpy(dev_a, host_a, N * sizeof(int),
+	cudaMemcpy(dev_a, host_a, N *M*sizeof(int),
 		cudaMemcpyHostToDevice);
-	cudaMemcpy(dev_b, host_b, N * sizeof(int),
+	cudaMemcpy(dev_b, host_b, N * sizeof(int)*M,
 		cudaMemcpyHostToDevice);
 	// вызов ядра
-	add << <1, N >> > (dev_a, dev_b, dev_c);
+	add << <1, N*M>>>(dev_a, dev_b, dev_c);
 	// получаем результат расчета
-	cudaMemcpy(host_c, dev_c, N * sizeof(int),
+	cudaMemcpy(host_c, dev_c, N * sizeof(int)*M,
 		cudaMemcpyDeviceToHost);
 	// вывод результатов
 	//for (int i = 0; i < N; i++)
@@ -55,9 +61,9 @@ int main() {
 	cudaFree(dev_a);
 	cudaFree(dev_b);
 	cudaFree(dev_c);
-    double end_time = clock(); // конечное время
-    double search_time = end_time - start_time; // искомое время
-	cout << N<<endl;
+	double end_time = clock(); // конечное время
+	double search_time = end_time - start_time; // искомое время
+	cout << N << endl << M << endl;
 	cout << search_time;
 	return 0;
 }
